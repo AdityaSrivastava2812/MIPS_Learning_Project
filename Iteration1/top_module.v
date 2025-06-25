5eb73c96262c0c92fed72968058d0c2142c87e7e
Aditya <aditya.srivastava2812@gmail.com>
2025-06-25 23:54:09 +0530

    Final update to top_module.v
    Fixed all naming errors and typos, ensuring it functions correctly.

--- Iteration1/top_module.v
+++ Iteration1/top_module.v
@@ -7,18 +7,18 @@ module top_module(
 // first lets start with PC updatation
 
 wire [31:0] pc_next,pc;
-pc inst( .clk(clk), .reset(reset), pc_next(pc_next) , .pc(pc));
+pc pc_inst(.clk(clk), .reset(reset), .pc_next(pc_next) , .pc(pc));
 // pc_next then goes to instruction fetch memory, which outputs a 32-bit instruction
 
 wire [31:0] instruction;
-inst_mem inst(.byte_address(pc_next), .instruction(instruction));
+instr_mem im_inst(.byte_address(pc_next), .instruction(instruction));
 //Now this instruction is broken into smaller vectors and processed in different modules, let's start with reg_file
 wire [31:0] data, read_data1, read_data2;
 wire [4:0] reg_mux;
 wire [31:0] write_to_reg_data;
 wire regWrite;
-register_file inst(.clk(clk), Read_reg1(instruction[25:21]), .Read_reg2(instruction[20:16]), .Write_reg(reg_mux), .Data(write_to_reg_data),
-                       .Read_data1(read_data1), .Read_data2(read_data2), .Regwrite(regWrite));
+Register_file rf_inst(.clk(clk), .Read_reg1(instruction[25:21]), .Read_reg2(instruction[20:16]), .Write_reg(reg_mux), .Data(write_to_reg_data),
+                       .Read_data1(read_data1), .Read_data2(read_data2), .RegWrite(regWrite));
 //Okay, now before I forget about these wires, let's create a mux for reg_mux
 wire regDst;
 assign reg_mux = regDst ? instruction[15:11]:instruction[20:16] ;
@@ -32,7 +32,7 @@ alu_control inst(.aluop(aluop), .funct(instruction[5:0]) , .result(alu_in));
 // now let's connect AlU
 wire [31:0] alu_result;
 wire zero;
-alu inst(.ctrl(alu_in) , .in1(read_data1), .in2(alusrc_mux) , .result(alu_result), .zero(zero));
+alu alu_inst(.ctrl(alu_in) , .in1(read_data1), .in2(alusrc_mux) , .result(alu_result), .zero(zero));
 // now again let's finish up with alusrc_mux before forgetting it
 wire [31:0] alusrc_mux;
 wire alusrc;
@@ -41,22 +41,22 @@ assign alusrc_mux = alusrc ? extnded_sgnl : read_data2;
 //Now let's connect the data memory
 wire memRead, memWrite;
 wire [31:0] mem_output;
-data_memory inst(.address(alu_result), .write_data(read_data2), .mem_read(memRead), .mem_write(memWrite), .clock(clk) , read_data(mem_output));
+data_memory dm_inst(.address(alu_result), .write_data(read_data2), .mem_read(memRead), .mem_write(memWrite), .clock(clk) , .read_data(mem_output));
 //Now let's finish up this path with write_to_reg_data mux
 wire memToReg;
 assign write_to_reg_data = memToReg ? mem_output : alu_result;
 
 // now connecting the control unit
 wire branch;
-control_unit inst(.opcode(instruction[31:26]), .regDst(regDst), .branch(branch) , .memRead(memRead),.memToReg(memToreg), .aluop(aluop), .memWrite(memWrite), .alusrc(alusrc), .regWrite(regWrite));
+control_unit ctrl_inst(.opcode(instruction[31:26]), .regDst(regDst), .branch(branch) , .memRead(memRead),.memToReg(memToreg), .aluop(aluop), .memWrite(memWrite), .alusrc(alusrc), .regWrite(regWrite));
 // now the final path for PC jump update
 wire [31:0] pc_branch;
 wire [31:0] update_pc;
 wire [31:0] word_extended_sgnl;
 assign word_extended_sgnl = extnded_sgnl << 2;
 assign update_pc = (pc_next + 4);
-assign pc_branch = update_pc + word_extnded_sgnl;
+assign pc_branch = update_pc + word_extended_sgnl;
 // now to choose what goes to pc
-assign pc = (branch & zero) ? pc_branch : update_branch;
+assign update_pc = (branch & zero) ? pc_branch : update_pc;
 //This completes the datapath.
 endmodule
